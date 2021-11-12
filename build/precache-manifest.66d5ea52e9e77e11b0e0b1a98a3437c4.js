12ed8613b854f4cbb978c892b42cf93786c22f71
cyberant28 <73520326+cyberant28@users.noreply.github.com>
2021-11-12 01:36:39 -0500

    adding files

---
+++ build/precache-manifest.66d5ea52e9e77e11b0e0b1a98a3437c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec55cfd7379031a116c3",
+    "url": "/cyberant28/comp3104/static/css/main.3a181c3f.chunk.css"
+  },
+  {
+    "revision": "ec55cfd7379031a116c3",
+    "url": "/cyberant28/comp3104/static/js/main.ec55cfd7.chunk.js"
+  },
+  {
+    "revision": "968b9afcd6a80d44c81f",
+    "url": "/cyberant28/comp3104/static/js/runtime~main.968b9afc.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/cyberant28/comp3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "d38534c9717a969476db9dfb336f5909",
+    "url": "/cyberant28/comp3104/index.html"
+  }
+];
